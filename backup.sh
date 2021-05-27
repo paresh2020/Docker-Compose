ddb6b4160dd016f6ae0d3f0c2ecccc4fbd8c865d
paresh patra <paresh.unix@gmail.com>
2021-05-27 23:29:23 +0530

    backup1

--- backup.sh
+++ backup.sh
@@ -7,4 +7,4 @@ PORT=3306
 MYSQL_USER="root"
 MYSQL_PASSWORD="root"
 #docker exec flaskdockercomposepipeline_db_1 bash -c 'exec mysqldump --databases "$MYSQL_DATABASE" -h<DOCKER_MYSQL_SERVICE_NAME> -u"$MYSQL_USER" -p"$MYSQL_PASSWORD"' > gzip > "$BACKUP_DIR"/"$BACKUP_FILE_NAME";
-docker exec flaskdockercomposepipeline_db_1 bash -c 'exec mysqldump  -h $DOCKER_MYSQL_SERVICE_NAME -P "$PORT" -u "$MYSQL_USER" -p --all-databases' > gzip > "$BACKUP_DIR"/"$BACKUP_FILE_NAME";
+docker exec flaskdockercomposepipeline_db_1 bash -c 'exec mysqldump  -h $DOCKER_MYSQL_SERVICE_NAME -P "$PORT" -u "$MYSQL_USER" -p --all-databases' > gzip > "$BACKUP_DIR"/"$BACKUP_FILE_NAME" || exit 0;
